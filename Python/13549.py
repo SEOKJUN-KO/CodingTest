ad975d21c17518a81d6ef63da48567a1ebee63ac
SEOKJUN KO <tjrwns1021@gmail.com>
2023-01-07 13:49:37 +0900

    Refactoring 13549
    same as title

--- Python/13549.py
+++ Python/13549.py
@@ -1,10 +1,6 @@
 from collections import deque
 
 N, K = map(int, input().split() )
-if(N == K):
-    print(0)
-    exit()
-
 arr = [-1]*(200001)
 
 def BFS():
@@ -16,20 +12,14 @@ def BFS():
         x, w = que.popleft()
         if(x == K):
             return w
-        ox = x-1
-        tx = x+1
-        thx = 2*x
-        if( 0 <= thx < 200000 ):
-            if( arr[thx] == -1 ):
-                arr[thx] = w
-                que.append([thx, w])
-        if( 0 <= ox < 100001 ):
-            if( arr[ox] == -1 ):
-                arr[ox] = w+1
-                que.append([ox, w+1])
-        if( 0 <= tx < 100001 ):
-            if( arr[tx] == -1 ):
-                arr[tx] = w+1
-                que.append([tx, w+1])
+        if( 0 <= 2*x < 200000 ):
+            if( arr[2*x] == -1 ):
+                arr[2*x] = w
+                que.append([2*x, w])
+        for X in (x-1, x+1):
+            if( 0 <= X < 100001 ):
+                if( arr[X] == -1 ):
+                    arr[X] = w+1
+                    que.append([X, w+1])
     
 print(BFS())
