f2f4206a0f332bd9f30101c8be241b47cc33934a
SEOKJUN KO <tjrwns1021@gmail.com>
2023-01-28 09:59:12 +0900

    Add 15651
    backtracking

---
+++ Python/15651.py
@@ -0,0 +1,13 @@
+N, M = map(int,input().split())
+
+arr = []
+def backtracking():
+    global N, M
+    if(len(arr) == M):
+        print(" ".join(map(str, arr)) )
+        return
+    for i in range(1, N+1):
+        arr.append(i)
+        backtracking()
+        arr.pop()
+backtracking()
