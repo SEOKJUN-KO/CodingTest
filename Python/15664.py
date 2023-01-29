bbf7542218c152086381ba0aca1ee4fe6352a5ea
SEOKJUN KO <tjrwns1021@gmail.com>
2023-01-29 20:54:09 +0900

    Add 15664
    backtracking

---
+++ Python/15664.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split() )
+arr = list(map(int, input().split() ))
+arr.sort()
+com = set([])
+ans = []
+isUsed = [False]*N
+
+def backtracking():
+    global N, M
+    if(len(ans) == M):
+        if(" ".join(map(str, ans)) not in com):
+            com.add(" ".join(map(str, ans)))
+            print(" ".join(map(str, ans)))
+        return
+    for i in range(N):
+        if(ans == []):
+            ans.append(arr[i])
+            isUsed[i] = True
+            backtracking()
+            isUsed[i] = False
+            ans.pop()
+        elif(ans[-1] <= arr[i] and not isUsed[i]):
+            ans.append(arr[i])
+            isUsed[i] = True
+            backtracking()
+            isUsed[i] = False
+            ans.pop()
+
+backtracking()
