4dff2e3dea4d98a0be78c907a084846a1a6ee252
Seokjun Ko <tjrwns1021@gmail.com>
2023-04-03 14:41:19 +0900

    [Add] 11724
    graph but use linked list

--- Python/11724.py
+++ Python/11724.py
@@ -24,3 +24,57 @@ for i in range(1, N+1):
         checkConnected(i)
         ans += 1
 print(ans)
+
+import sys
+from collections import deque
+N, M = map(int, sys.stdin.readline().split())
+
+visited = [False]*(N+1)
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+class LList:
+    def __init__(self):
+        self.head = None
+    def addFirst(self, val):
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+    def mprint(self):
+        node = self.head
+        while node is not None:
+            print(node.val, end=" ")
+            node = node.next
+    def searchNode(self):
+        node = self.head
+        while node is not None:
+            if(visited[node.val] == False):
+                visited[node.val] = True
+                return node.val
+            node = node.next
+        return 0
+A = [LList() for _ in range(N+1)]
+
+for j in range(M):
+    V1, V2 = map(int, sys.stdin.readline().split())
+    A[V1].addFirst(V2)
+    A[V2].addFirst(V1)
+
+def checkConnected(V):
+    que = deque()
+    que.append(V)
+    visited[V] = True
+    while(que):
+        now = que.popleft()
+        while(True):
+            O = A[now].searchNode()
+            if(O == 0):
+                break
+            que.append(O)
+ans = 0
+for i in range(1, N+1):
+    if(visited[i] == False):
+        checkConnected(i)
+        ans += 1
+print(ans)
