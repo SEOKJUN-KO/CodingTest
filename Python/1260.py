fc98a411f98bef00cb5ee17489960f937f4f7f99
SEOKJUN KO <64264896+SEOKJUN-KO@users.noreply.github.com>
2023-04-04 19:54:28 +0900

    Create 1260.py
    solving graph problem, using bfs and dfs

---
+++ Python/1260.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+n,m,start=map(int,input().split())
+visited=[False]*(n+1)
+
+lList=[[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    lList[a].append(b)
+    lList[b].append(a)
+for i in range(1, n+1):
+    lList[i] = sorted(lList[i])
+
+def dfs(start):
+    print(start,end=' ')
+    visited[start]=True
+    for i in lList[start]:
+        if not visited[i]:
+            dfs(i)
+            visited[i]=True
+
+def bfs(start):
+    q=deque([start])
+    visited[start]=True
+    while q:
+        now=q.popleft()
+        print(now,end=' ')
+        for i in lList[now]:
+            if not visited[i]:
+                q.append(i)
+                visited[i]=True
+dfs(start)
+visited=[False]*(n+1)
+print()
+bfs(start)
