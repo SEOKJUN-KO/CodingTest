84ffb3cf0d9596f8fceb45509bf1df4165d06927
Seokjun Ko <tjrwns1021@gmail.com>
2024-10-06 22:07:29 +0900

    [FIX] 색깔 트리
    객체 지향 코드

--- Python/시뮬레이션/색깔 트리.py
+++ Python/시뮬레이션/색깔 트리.py
@@ -1,87 +1,70 @@
 import sys
+input = sys.stdin.readline
 
-rootDic = {}
-nodeDic = {}
-
+score = 0
 class Node:
-    def __init__(self, mid, color, maxDepth):
+    def __init__(self, mid, pid, color, maxDepth):
         self.mid = mid
+        self.pid = pid
         self.color = color
         self.maxDepth = maxDepth
-        self.child = []
+        self.childList = []
+        
+    def addChild(self, node):
+        if self.maxDepth == 1: return False
+        depth = min(self.maxDepth-1, node.maxDepth)
+        node.changeDepth(depth)
+        self.childList.append(node)
+        return True
 
-def addNode(mid, pid, color, maxDepth):
-    node = Node(mid, color, maxDepth)
-    if pid == -1:
-        rootDic[mid] = node
-        return
-    flag = False
-    depth = maxDepth
-    if pid in rootDic.keys():
-        pNode = rootDic[pid]
-        depth = min(depth, pNode.maxDepth-1)
-        node.maxDepth = depth
-        if depth >= 1:
-            pNode.child.append(mid)
-            flag = True
-    else:
-        depth = min(depth, nodeDic[pid].maxDepth-1)
-        node.maxDepth = depth
-        if depth >= 1:
-            nodeDic[pid].child.append(mid)
-            flag = True
-    if flag:
-        nodeDic[mid] = node
+    def changeDepth(self, depth):
+        self.maxDepth = depth
 
-def changeColor(mid, color):
-    if mid in rootDic.keys():
-        rootDic[mid].color = color
-        for child in rootDic[mid].child:
-            changeColor(child, color)
-        return
-    nodeDic[mid].color = color
-    for child in nodeDic[mid].child:
-        changeColor(child, color)
-    return
-
-def viewColor(mid):
-    if mid in rootDic.keys():
-        print(rootDic[mid].color)
-        return
-    print(nodeDic[mid].color)
-
-cnt = 0
-def calculate(mid):
-    global cnt
-    s = set()
-    if mid == -1:
-        for rid in rootDic.keys():
-            for child in rootDic[rid].child:
-                s |= calculate(child)
-            s.add(rootDic[rid].color)
-            cnt += len(s)*len(s)
-            s = set()
-        return
-    for child in nodeDic[mid].child:
-        s |= calculate(child)
-    s.add(nodeDic[mid].color)
-    cnt += len(s)*len(s)
-    return s
+    def changeColor(self, color):
+        self.color = color
+        for child in self.childList:
+            child.changeColor(color)
     
+    def showColor(self):
+        print(self.color)
 
-input = sys.stdin.readline
+    def calculateScore(self):
+        global score
+        s = set([])
+        s.add(self.color)
+        for child in self.childList: s |= child.calculateScore()
+        score += len(s)*len(s)
+        return s
+
+nodeD = {}
+rootD = {}
+def makeNode(mid, pid, color, maxDepth):
+    node = Node(mid, pid, color, maxDepth)
+    if pid == -1:
+        rootD[mid] = node
+        nodeD[mid] = node
+        return
+    parent = nodeD[pid]
+    if parent.addChild(node):
+        nodeD[mid] = node
+
 Q = int(input())
 for _ in range(Q):
-    order = list(map(int, input().split(" ")))
-    if order[0] == 100:
-        (mid, pid, color, maxDepth) = (order[1], order[2], order[3], order[4])
-        addNode(mid, pid, color, maxDepth)
-    elif order[0] == 200:
-        (mid, color) = (order[1], order[2])
-        changeColor(mid, color)
-    elif order[0] == 300:
-        viewColor(order[1])
-    else:
-        cnt = 0
-        calculate(-1)
-        print(cnt)
+    inp = list(map(int, input().split(" ")))
+    if inp[0] == 100:
+        mid, pid, color, maxDepth = inp[1], inp[2], inp[3], inp[4]
+        makeNode(mid, pid, color, maxDepth)
+    elif inp[0] == 200:
+        mid, color = inp[1], inp[2]
+        node = nodeD[mid]
+        node.changeColor(color)
+    elif inp[0] == 300:
+        mid = inp[1]
+        node = nodeD[mid]
+        node.showColor()
+    elif inp[0] == 400:
+        score = 0
+        for root in rootD.keys():
+            node = nodeD[root]
+            node.calculateScore()
+        print(score)
