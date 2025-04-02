dec5d264cd8858412c079c399702b0d01b34d52e
Seokjun Ko <tjrwns1021@gmail.com>
2025-04-02 15:57:42 +0900

    [Fix] 코드 수정

--- Python/비트 연산/11723.py
+++ Python/비트 연산/11723.py
@@ -1,11 +1,25 @@
-def solution(n, arr1, arr2):
-    ans = []
-    for i in range(n):
-        tmp = ""
-        st = bin(( arr1[i] | arr2[i] ))[2:]
-        if len(st) < n: tmp += " "*(n-len(st))
-        for c in st:
-            if c == "1": tmp += "#"
-            else: tmp += " "
-        ans.append(tmp)
-    return ans
+import sys
+input = sys.stdin.readline
+state = 0
+
+m = int(input())
+    
+for _ in range(m):
+    data = input().split()  # 입력을 한 번에 읽고 공백 기준으로 분할
+    command = data[0]
+    if command == "add":
+        x = int(data[1])
+        state |= (1 << (x - 1))
+    elif command == "remove":
+        x = int(data[1])
+        state &= ~(1 << (x - 1))
+    elif command == "check":
+        x = int(data[1])
+        print(1 if (state >> (x - 1)) & 1 else 0)
+    elif command == "toggle":
+        x = int(data[1])
+        state ^= (1 << (x - 1))
+    elif command == "all":
+        state = 0xfffff
+    elif command == "empty":
+        state = 0
