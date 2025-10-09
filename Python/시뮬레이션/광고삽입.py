976812629eee598359baa99e45efcf07c95d03c9
Seokjun Ko <tjrwns1021@gmail.com>
2025-10-09 15:00:40 +0900

    [Fix] 효율성 개선

--- Python/시뮬레이션/광고삽입.py
+++ Python/시뮬레이션/광고삽입.py
@@ -1,32 +1,14 @@
-from heapq import heappush, heappop
-
 def changeToSec(timeStr):
     h, m, s = map(int, timeStr.split(":"))
     return (h*60*60) + (m*60) + s
 
-def makeLogsArr(logs): # 3*10^5
+def makeLogsArr(logs):
     arr = []
     for log in logs:
         A, B = log.split("-")
         arr.append([changeToSec(A), changeToSec(B)])
     arr = sorted(arr, key = lambda x: (x[0], x[1]) )
     return arr
-
-def makeTerms(logArr, advT, playT): # 6*10^5
-    arr = []
-    arr.append([0, advT])
-    for s, e in logArr:
-        arr.append([s, max(s+advT, playT)])
-        arr.append([max(0, e-advT), e])
-    arr.append([max(0, playT-advT), playT])
-    arr = sorted(arr, key = lambda x: (x[0], x[1]) )
-    return arr
-
-def addTime(startT, endT, heap):
-    tmp = 0
-    for e, s in heap:
-        tmp += min(endT, e) - max(s, startT)
-    return tmp
 
 def secToStr(time):
     h = time//3600
@@ -41,33 +23,39 @@ def secToStr(time):
     if len(s) == 1: s = '0'+s
     return h+":"+m+":"+s
 
-def printAll(heap):
-    for e, s in heap:
-        print([secToStr(e), secToStr(s)], end=" ")
-
 def calculate(play_time, adv_time, logs):
     logArr = makeLogsArr(logs)
     advT = changeToSec(adv_time)
     playT= changeToSec(play_time)
-    terms = makeTerms(logArr, advT, playT)
     
-    heap = []
-    answer = [0, changeToSec('100:0:0')]
-    lastP = 0
-    for i in range(len(terms)): #6*10^6
-        s, e = terms[i]
-        while(heap and heap[0][0] < s):
-            heappop(heap)
-        while(lastP < len(logArr) and logArr[lastP][0] <= s+advT ):
-            ee = logArr[lastP][1]
-            ss = logArr[lastP][0]
-            heappush(heap, (ee, ss))
-            lastP += 1
-        tmp = addTime(s, s+advT, heap) # -> 이게 문제일수도?
-        if answer[0] < tmp: 
-            answer = [tmp, secToStr(s)]
-        if s+advT >= playT: break
-    return answer[1]
+    if advT >= playT: return "00:00:00"
+    
+    times = [0 for _ in range(playT+2)]
+    for s, e in logArr:
+        times[s] += 1
+        if e < playT:     
+            times[e] -= 1  
+    
+    dp = [0 for _ in range(playT+2)]
+    dp[0] = times[0]
+    for i in range(1, playT+1):
+        dp[i] = dp[i-1] + times[i]
+    
+    answer = [-1, playT]
+    sumT = 0
+
+    for i in range(advT):
+        sumT += dp[i]
+    if sumT > answer[0]:
+        answer = [sumT, 0]
+    
+    for t in range(advT, playT):
+        sumT += dp[t]
+        sumT -= dp[t-advT]
+        if sumT > answer[0]:
+            answer = [sumT, t-advT+1]
+        
+    return secToStr(answer[1])
     
 def solution(play_time, adv_time, logs):
     return calculate(play_time, adv_time, logs)
